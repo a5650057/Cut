297afdb2424bf19157c649602c493471e335fe34
a5650057 <a5650057@gmail.com>
2023-05-15 15:57:03 +0800

    message

--- cut.py
+++ cut.py
@@ -39,10 +39,10 @@ def split_text(text, max_tokens=None):
         max_tokens =  3050
         sentences = re.split(r'(?<=[。！？])\s*', text)
     elif language == 'en':
-        max_tokens =  4100
+        max_tokens =  3600
         sentences = re.split(r'(?<=[.])\s+', text)
     else:
-        max_tokens = 3050  # 預設情況下，將使用中文的 token 限制
+        max_tokens = 3050
         sentences = re.split(r'(?<=[。！？])\s*', text)
 
     result = []
@@ -60,9 +60,6 @@ def split_text(text, max_tokens=None):
         result.append(current_chunk.strip())
 
     return result
-
-# 其他部分保持不變
-
 
 
 def open_file():
@@ -88,14 +85,23 @@ def open_file():
     max_tokens = 3050
     split_result = split_text(text, max_tokens)
 
+
+    part_contents.clear()
+    for widget in copy_button_frame.winfo_children():
+        widget.destroy()
+
     for idx, part in enumerate(split_result):
         part_tokens = count_tokens(part)
         text_box.insert(tk.END, f"Part {idx + 1} :\n\n{part}\n\n")
         text_box.tag_configure(f"tag{idx + 1}", foreground="blue", underline=True)
         part_selection_menu['menu'].add_command(label=f"Part {idx + 1}", command=lambda idx=idx: jump_to_part(idx))
 
-    print(f"Split the text into {len(split_result)} parts")  # 添加這一行
+        part_contents[idx] = f"Part {idx + 1} :\n\n{part}\n\n"
+        copy_button = tk.Button(copy_button_frame, text=f"Copy Part {idx + 1}",
+                                command=lambda idx=idx: copy_part_to_clipboard(idx))
+        copy_button.pack(side=tk.LEFT)
 
+    print(f"Split the text into {len(split_result)} parts")
 
 
 
@@ -105,11 +111,16 @@ def jump_to_part(part_index):
     text_box.tag_add("sel", line_number, f"{line_number}+1line")
     text_box.see(line_number)
 
-
+def copy_part_to_clipboard(part_index):
+    root.clipboard_clear()
+    root.clipboard_append(part_contents[part_index])
 
 def refresh_ui():
     text_box.delete("1.0", tk.END)  # 清空文字區塊
     part_selection_menu['menu'].delete(0, 'end')  # 清空選單
+    for widget in copy_button_frame.winfo_children():
+        widget.destroy()
+
 
 root = tk.Tk()
 root.title("分割Tokens")
@@ -117,14 +128,18 @@ root.title("分割Tokens")
 open_button = tk.Button(root, text="Open File", command=open_file)
 open_button.pack()
 
-# 新增的刷新按鈕
 refresh_button = tk.Button(root, text="Refresh", command=refresh_ui)
-refresh_button.pack()  # 將刷新按鈕加入視窗
+refresh_button.pack()
 
 text_box = tk.Text(root)
 text_box.pack()
 
 part_selection_menu = tk.OptionMenu(root, tk.StringVar(), "")
 part_selection_menu.pack()
+
+copy_button_frame = tk.Frame(root)
+copy_button_frame.pack()
+
+part_contents = {}
 
 root.mainloop()
